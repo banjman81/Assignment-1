987cce5ba85a412dc4e7f3efe6ef118c144a0b39
banjman81 <70820423+banjman81@users.noreply.github.com>
2022-01-24 15:49:03 -0600

    assignment completed

--- q7-q11.js
+++ q7-q11.js
@@ -2,10 +2,27 @@ const x = 6
 
 // 7. Write a function that takes 2 numbers as arguments and returns the sum of both numbers and the variable "x" using without using arrow functions.
 
+  function addXToResultNoArrow(input1, input2){
+    return input1 + input2 + x
+  }
+
+console.log("Question 7.", addXToResultNoArrow(1,2),)
+
 // 8. Write a function that takes 2 numbers as arguments and returns the sum of both numbers and the variable "x", using arrow functions.
+
+  const addXToResultArrow = (input1, input2) => {
+    return input1 + input2 + x
+  }
+
+  console.log("Question 8.", addXToResultArrow(5,2))
 
 // 9. Write a function that returns another function. (use arrow functions please)
 
+  const callBack = () => {
+    return addXToResultArrow
+  }
+
+  console.log("Question 9.", callBack()(6,6))
 
 // 10. given the following code:
 
@@ -21,6 +38,7 @@ const getFunction = () => {
 console.log(getFunction()(2))
 
 // Explain why the function that returns from getFunction still has access to "y" if "y" is not a global variable.
+  // It have access to the local variable fo getFunction because JS closures.
 
 
 
@@ -36,5 +54,19 @@ const couldThrowError = () => {
   
   return 'success'
 }
+
+const errorChecker = () => {
+  const message = couldThrowError()
+  return message
+}
+
+try{
+  console.log("Question 11.", errorChecker())
+  
+}catch(e){
+  console.log("Question 11.", e.message)
+}
+  
+
 
 
